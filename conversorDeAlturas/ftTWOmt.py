1162c1b782b01e8699f6b91af4fc26ae9be2d439
Hector Ponsoy <hectorjosue65@gmail.com>
2021-04-26 23:51:36 -0600

    GUI Finished

---
+++ conversorDeAlturas/ftTWOmt.py
@@ -0,0 +1,48 @@
+from tkinter import Tk, Button, Label, DoubleVar, Entry
+
+window = Tk()
+window.title('Pies a metros')
+window.configure(background='sky blue')
+window.geometry('320x220')
+window.resizable(width=False, height=False)
+
+ft_label = Label(window, text='Feet', bg='purple', fg='white', width=14)
+ft_label.grid(column=0, row=0, padx=15, pady=15)
+
+ft_val = DoubleVar()
+ft_entry = Entry(window,textvariable=ft_val, width=14)
+ft_entry.grid(column=1, row=0)
+ft_entry.delete(0, 'end')
+
+mt_label = Label(window, text='Meter', bg='purple', fg='white', width=14)
+mt_label.grid(column=0, row=1)
+
+mt_val = DoubleVar()
+mt_entry = Entry(window,textvariable=mt_val, width=14)
+mt_entry.grid(column=1, row=1, pady=30)
+mt_entry.delete(0, 'end')
+
+btnConvert = Button(window,text='Convert', bg='blue', fg='white', width=14, command='')
+btnConvert.grid(column=0, row=3, padx=15)
+
+btnClear = Button(window,text='Clear', bg='Black', fg='white', width=14, command='')
+btnClear.grid(column=1, row=3, padx=15)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
